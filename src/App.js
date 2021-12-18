9b090c84a5c1fda12967c397fb073ad5fd9d1ce3
Paul John Gonzales <paulj_gonzales@hotmail.com>
2021-12-18 14:14:49 -0500

    Created header component that links to pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div >
+      { 
+        /* Header/Navigation must be out of the Routes component so that it is 
+           always rendered regardless of which path the page is on */
+      }
+      <Header />
       <Routes>
           <Route path='/' element={ <Homepage />}/>
           <Route path='shop' element={ <ShopPage /> }/>
