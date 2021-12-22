b2b6cd64534f069863812ae413cc499287942732
Paul John Gonzales <paulj_gonzales@hotmail.com>
2021-12-22 11:04:51 -0500

    Initial heroku commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,13 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+// create-react-app creates an environment variable called NODE_ENV that lets us check if the environment we are on is dev or prod
+// Only use redux-logger when we are in our dev environment
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 // To make the code more scalable, apply the spread operator to applyMiddleware
 // Anytime we need to add more middleware we just add more to the array of middlewares.
