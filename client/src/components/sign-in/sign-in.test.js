3ddac6b539999e4e797d0a5ee694b757b288e69f
Paul John Gonzales <paulj_gonzales@hotmail.com>
2022-02-15 21:09:34 -0500

    Fixed unit tests for sign-in component

--- client/src/components/sign-in/sign-in.test.js
+++ client/src/components/sign-in/sign-in.test.js
@@ -4,30 +4,66 @@ import { mount, shallow } from "enzyme";
 import * as redux from 'react-redux';
 import SignIn from "./sign-in";
 import FormInput from "../form-input/form-input";
+import CustomButton from "../custom-button/custom-button";
 
 describe('Sign In component', () => {
     let wrapper;
 
+    // Mock dispatch call
     const useDispatchMock = jest.spyOn(redux, 'useDispatch');
     const dispatch = jest.fn();
+
     beforeEach(() => {
         useDispatchMock.mockImplementation(() => dispatch);
         wrapper = shallow(<SignIn />);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
     it('should render sign-in component', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it('should submit form on click and dispatch', () => {
+    it('should submit form on click and dispatch EMAIL_SIGN_IN_START action', () => {
         expect.assertions(1);
+        
+        wrapper.find(FormInput).at(0).dive().find('.form-input').simulate('change', {
+            target: {
+                name: 'email',
+                value: 'foo@gmail.com'
+            }
+        });
+
+        wrapper.find(FormInput).at(1).dive().find('.form-input').simulate('change', {
+            target: {
+                name: 'password',
+                value: 'test123'
+            }
+        });
+
         wrapper.find('form').simulate(
             'submit', {
                 preventDefault() {}
             }
         );
+        expect(dispatch).toHaveBeenCalledWith({
+            type: 'EMAIL_SIGN_IN_START',
+            payload: {
+                email: 'foo@gmail.com',
+                password: 'test123'
+            }
+        });
+    });
 
-        expect(dispatch).toHaveBeenCalled();
+    it('should dispatch GOOGLE_SIGN_IN_START action upon clicking second custom button', () => {
+
+        wrapper.find(CustomButton).at(1).simulate('click');
+
+        expect(dispatch).toHaveBeenCalledWith({
+            type: 'GOOGLE_SIGN_IN_START'
+        });
     });
 
     it('should update email state on change of email input', () => {
